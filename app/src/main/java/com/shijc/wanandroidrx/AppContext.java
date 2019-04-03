4b2d8f556d87218bddca5fe8c18a25157eec4d5f
shijiacheng <iot_shijiacheng@163.com>
2019-04-03 11:25:47 +0800

    加入leak库

--- app/src/main/java/com/shijc/wanandroidrx/AppContext.java
+++ app/src/main/java/com/shijc/wanandroidrx/AppContext.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 import com.shijc.wanandroidrx.common.base.Preference;
+import com.squareup.leakcanary.LeakCanary;
 import org.litepal.LitePal;
 
 /**
@@ -24,6 +25,12 @@ public class AppContext extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
         instance = this;
         Preference.getInstance(this,"config");
         LitePal.initialize(this);
